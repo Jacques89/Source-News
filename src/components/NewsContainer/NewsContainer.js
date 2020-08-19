81f997002f9a962f0d7e275030ed4a5352c02520
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2020-08-19 14:28:17 +0200

    First commit

---
+++ src/components/NewsContainer/NewsContainer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../../styles/NewsContainer.scss'
+
+const Navigation = () => {
+  return (
+    <nav>
+      <h1>Source News</h1>
+      <h2>Straight from the sources mouth!</h2>
+      <ul>
+        <li><a href='#'>Politics</a></li>
+        <li><a href='#'>Sport</a></li>
+        <li><a href='#'>Economics</a></li>
+        <li><a href='#'>Culture</a></li>
+        <li><a href='#'>Tech</a></li>
+      </ul>
+    </nav>
+  )
+}
