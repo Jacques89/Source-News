8194334fa06d04cdf40402c53dc11d90ac6b68ac
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2020-08-19 16:36:05 +0200

    Added axios and api fetch method

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,13 +1,44 @@
-import React from 'react'
-import '../../styles/NewsContainer.scss'
+import React, { Component } from 'react'
 
+import axios from "axios";
 
-const NewsContainer = () => {
-  return (
-      <div className="container">
-          
-      </div>
-  )
+import NewsBox from '../NewsBox/NewsBox'
+import "../../styles/NewsContainer.scss";
+
+export default class ContainerOfNews extends Component {
+  state = ({
+      news: []
+  })
+
+  componentDidMount = async () => {
+      try {
+          const serverData = await axios('https://newscafapi.p.rapidapi.com/apirapid/news/world/?q=news&apikey=5fc79354damshc76091ac5b0282cp1a5221jsn2ab34185811c');
+          const response = await serverData.json();
+          this.setState({
+              news: response.articles
+          })
+      } catch (error) {
+          alert(error)
+      }
+  }
+
+  render() {
+      const { news } = this.state
+      let articles;
+      if (news.length > 0) {
+          articles = news.map((article, i) => {
+              if (!article.description.includes('https')) {
+                  return (
+                      <div className="box" key={i}><NewsBox key={i} article={article} /></div>
+                  )
+              }
+          })
+      }
+    return (
+        <div className="container" >
+            {articles}
+
+        </div>
+    )
+  } 
 }
-
-export default NewsContainer
