8425ad0862980cae8a462bd238f6a218bb1ef1ee
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2020-09-03 16:43:27 +0200

    removed jest mock fetch package

--- src/setUpTests.js
+++ src/setUpTests.js
@@ -1,7 +1,4 @@
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import fetchMock from 'jest-fetch-mock'
  
 Enzyme.configure({ adapter: new Adapter() })
-
-fetchMock.enableMocks()
