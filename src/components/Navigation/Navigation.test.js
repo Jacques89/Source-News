8f241ed1412844cb9c73281787836c5841fb848f
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2020-10-26 17:57:42 +0100

    More refactoring of Nav test.js, removed beforeEach

--- src/components/Navigation/Navigation.test.js
+++ src/components/Navigation/Navigation.test.js
@@ -6,22 +6,38 @@ import userEvent from '@testing-library/user-event'
 
 describe('NavigationBar', () => {
   const changeCategory = jest.fn()
-
-  beforeEach(() => {
-    render(<Navigation changeCategory={changeCategory} />)
-  })
+  const menuOpen = jest.fn()
 
   it('renders a title and sub-title', () => {
+    render(
+      <Navigation 
+        menuOpen={false} 
+        changeCategory={changeCategory} 
+      />
+    )
     expect(screen.getByRole('heading', {name: /Source News/i }))
     expect(screen.getByRole('heading', {name: /Straight from the sources mouth!/i }))
   })
 
   it('renders news categories', () => {
+    render(
+      <Navigation 
+        menuOpen={false} 
+        changeCategory={changeCategory} 
+      />
+    )
     expect(screen.getByTestId('news-categories')).toMatchSnapshot()
   })
 
   it('changes category when a category is clicked', () => {
+    render(
+      <Navigation 
+        menuOpen={true} 
+        changeCategory={changeCategory} 
+      />
+    )
     userEvent.click(screen.getByTestId('business'))
     expect(changeCategory).toHaveBeenCalledTimes(1)
+    expect(menuOpen).toBeTruthy()
   })
 })
