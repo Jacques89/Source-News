bb41ee492496bb0db0dbe52d0fd481a94a68a006
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2021-10-19 12:47:09 +0200

    Updated app.js quotes

--- src/App.test.js
+++ src/App.test.js
@@ -1,36 +1,36 @@
-import React from "react"
-import App from "./App"
-import { setupFetchStub } from "../__mocks__/Fetch"
+import React from 'react'
+import App from './App'
+import { setupFetchStub } from '../__mocks__/Fetch'
 
-import { render, screen, act } from "@testing-library/react"
+import { render, screen, act } from '@testing-library/react'
 
-describe("App", () => {
-  it("should render", async () => {
+describe('App', () => {
+  it('should render', async () => {
     await act(async () => render(<App />))
-    expect(screen.getByTestId("app")).toMatchSnapshot()
+    expect(screen.getByTestId('app')).toMatchSnapshot()
   })
 })
 
-describe("API call", () => {
+describe('API call', () => {
   const data = [
     {
-      category: "World",
-      content: "Squirrel becomes President",
-      img: "some Image URL",
-      title: "Squirrel has now become President of the World",
+      category: 'World',
+      content: 'Squirrel becomes President',
+      img: 'some Image URL',
+      title: 'Squirrel has now become President of the World'
     },
     {
-      category: "Sports",
-      content: "Messi wins World Cup with Argentina",
-      img: "some url image",
-      title: "Messi finally wins a World Cup",
-    },
+      category: 'Sports',
+      content: 'Messi wins World Cup with Argentina',
+      img: 'some url image',
+      title: 'Messi finally wins a World Cup'
+    }
   ]
 
-  it("fetches data from server when server returns a successful response", async () => {
+  it('fetches data from server when server returns a successful response', async () => {
     const url = process.env.REACT_APP_NEWS_API
     const fakeData = { news: data }
-    jest.spyOn(global, "fetch").mockImplementation(setupFetchStub(fakeData))
+    jest.spyOn(global, 'fetch').mockImplementation(setupFetchStub(fakeData))
 
     const res = await fetch(url)
     const json = await res.json()
