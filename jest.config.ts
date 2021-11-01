b055cae72b8eeb0a4d3770f406a275ca01e31fa6
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2021-11-01 11:47:12 +0100

    Remodified testfiles with

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from '@jest/types'
+const { defaults } = require('jest-config')
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  verbose: true,
+  testMatch: ['<rootDir>/**/__tests__/**/*(*.)+(test).+(tsx|ts)'],
+  setupFiles: ['dotenv/config'],
+  setupFilesAfterEnv: ['./src/setupTests.ts'],
+  moduleFileExtensions: ['js', 'ts', 'tsx'],
+  collectCoverage: true,
+  coverageDirectory: 'target/coverage',
+  collectCoverageFrom: ['src/**/*.tsx'],
+  moduleNameMapper: {
+    '^.+\\.(css|scss)$': 'identity-obj-proxy',
+    '^.+\\.(png|svg|pdf|jpg|jpeg)$': 'jest-transform-stub',
+    '^@foo/(.*)': '<rootDir>/src/$1'
+  }
+}
+
+export default config
