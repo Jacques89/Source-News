cf65df61687a849f3595148cc720a52b563d6cab
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2021-11-03 12:11:08 +0100

    Added @testinglibrary/jest-dom/extend expect directly into jest config

--- jest.config.ts
+++ jest.config.ts
@@ -2,15 +2,19 @@ import type { Config } from '@jest/types'
 import { defaults } from 'jest-config'
 
 const config: Config.InitialOptions = {
+  roots: ['<rootDir>/src/__tests__', '<rootDir>/src'],
   transform: {
-    ".(ts|tsx)": "ts-jest"
+    '.(ts|tsx)': 'ts-jest'
   },
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   verbose: true,
   moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-  testMatch: ['<rootDir>/**/__tests__/**/*(*.)+(test).+(tsx|ts)', '<rootDir>/src/**/*(*.)+(test).+(tsx|ts)'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  testMatch: [
+    '<rootDir>/**/__tests__/**/*(*.)+(test).+(tsx|ts)',
+    '<rootDir>/src/**/*(*.)+(test).+(tsx|ts)'
+  ],
   moduleNameMapper: {
     '^.+\\.(css|scss)$': 'identity-obj-proxy'
   }
