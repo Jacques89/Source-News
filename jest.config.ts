b6c109527e7507ab4fcf85549ffa3857c69f139b
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2021-11-03 11:01:16 +0100

    added rootDir to setupFIlesAfterEnv

--- jest.config.ts
+++ jest.config.ts
@@ -6,7 +6,7 @@ const config: Config.InitialOptions = {
   testEnvironment: 'jsdom',
   verbose: true,
   moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
-  setupFilesAfterEnv: ['./src/setupTests.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   testMatch: ['<rootDir>/**/__tests__/**/*(*.)+(test).+(tsx|ts)', '<rootDir>/src/**/*(*.)+(test).+(tsx|ts)'],
   moduleNameMapper: {
     '^.+\\.(css|scss)$': 'identity-obj-proxy'
