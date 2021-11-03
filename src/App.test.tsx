727a27934201d40399e80a51f87eebbe89f752c8
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2021-11-03 13:32:40 +0100

    Wrapped waitForElementToBeRemoved in act, updated devDependencies

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -21,56 +21,59 @@ describe('App', () => {
     act(() => {
       render(<App />)
     })
-    await waitForElementToBeRemoved(() => screen.queryByText(/loading/i))
+    await act(async () => {
+      await waitForElementToBeRemoved(screen.queryByText(/loading/i))
+    })
     expect(screen.getByTestId('app')).toBeInTheDocument()
   })
-})
+  describe('API Request', () => {
+    beforeEach(() => {
+      global.fetch = jest
+        .fn()
+        .mockImplementation(setupFetchStub(mockNewsResponse))
+      // Clear
+      const fetchSpy = jest.spyOn(global, 'fetch')
+      fetchSpy.mockClear()
+    })
 
-describe('API Request', () => {
-  beforeEach(() => {
-    global.fetch = jest.fn().mockImplementation(setupFetchStub(mockNewsResponse))
-    // Clear
-    const fetchSpy = jest.spyOn(global, 'fetch')
-    fetchSpy.mockClear()
-  })
+    const mockNewsResponse: Array<NewsProps> = [
+      {
+        category: 'World',
+        content: 'Squirrel becomes President',
+        img: 'some Image URL',
+        title: 'Squirrel has now become President of the World'
+      },
+      {
+        category: 'Sports',
+        content: 'Messi wins World Cup with Argentina',
+        img: 'some url image',
+        title: 'Messi finally wins a World Cup'
+      }
+    ]
 
-  const mockNewsResponse: Array<NewsProps> = [
-    {
-      category: 'World',
-      content: 'Squirrel becomes President',
-      img: 'some Image URL',
-      title: 'Squirrel has now become President of the World'
-    },
-    {
-      category: 'Sports',
-      content: 'Messi wins World Cup with Argentina',
-      img: 'some url image',
-      title: 'Messi finally wins a World Cup'
-    }
-  ]
+    const url: string = `${process.env.REACT_APP_TEST_URL}`
 
-  const url: string = `${process.env.REACT_APP_TEST_URL}`
+    it('fetches data from API successfully', async () => {
+      const fakeData: Array<NewsProps> = mockNewsResponse
+      const fetchSpy = jest
+        .spyOn(global, 'fetch')
+        .mockImplementation(setupFetchStub(fakeData))
 
-  it('fetches data from API successfully', async () => {
-    const fakeData: Array<NewsProps> = mockNewsResponse
-    const fetchSpy = jest
-      .spyOn(global, 'fetch')
-      .mockImplementation(setupFetchStub(fakeData))
+      const res: Response = await fetch(url)
+      const json: Array<NewsProps> = await res.json()
+      expect(json).toEqual({ news: fakeData })
+      expect(fetchSpy).toHaveBeenCalledTimes(1)
+    })
+    it('returns an empty array if no data is available from API', async () => {
+      const emptyData: Array<NewsProps> = []
+      const fetchSpy = jest
+        .spyOn(global, 'fetch')
+        .mockImplementation(setupFetchStub(emptyData))
 
-    const res: Response = await fetch(url)
-    const json: Array<NewsProps> = await res.json()
-    expect(json).toEqual({ news: fakeData })
-    expect(fetchSpy).toHaveBeenCalledTimes(1)
-  })
-  it('returns an empty array if no data is available from API', async () => {
-    const emptyData: Array<NewsProps> = []
-    const fetchSpy = jest
-      .spyOn(global, 'fetch')
-      .mockImplementation(setupFetchStub(emptyData))
-
-    const res: Response = await fetch(url)
-    const json: Array<NewsProps> = await res.json()
-    expect(json).toEqual({ news: emptyData })
-    expect(fetchSpy).toHaveBeenCalledTimes(1)
+      const res: Response = await fetch(url)
+      const json: Array<NewsProps> = await res.json()
+      expect(json).toEqual({ news: emptyData })
+      expect(fetchSpy).toHaveBeenCalledTimes(1)
+    })
   })
 })
